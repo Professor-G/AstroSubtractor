87cab5ed1606d258ce7388df10d7363c27cadc41
Daniel <daniel@Daniels-MacBook-Pro-5.local>
2023-05-23 19:46:29 -0700

    module name change

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ Created on Fri June 6 3 16:40:44 2022
 
 @author: danielgodinez
 """
+
 from setuptools import setup, find_packages, Extension
 
-
 setup(
-    name="AstroSubtract",
+    name="AstroSubtractor",
     version="0.1",
     author="Daniel Godines",
     author_email="danielgodinez123@gmail.com",
     description="Machine learning classifier",
     license='GPL-3.0',
-    url = "https://github.com/Professor-G/AstroSubtract",
+    url = "https://github.com/Professor-G/AstroSubtractor",
     classifiers=[
 		'Development Status :: 5 - Production/Stable',
 		'Intended Audience :: Developers',
@@ -23,7 +23,9 @@ setup(
 		'Programming Language :: Python :: 3',	   
 ],
     packages=find_packages('.'),
-    install_requires = ['numpy'],
+    install_requires = ['numpy','tensorflow','scikit-learn','scikit-optimize','scipy','photutils', 
+        'matplotlib', 'progress', 'pandas', 'optuna', 'boruta', 'astropy', 'xgboost', 'BorutaShap',
+        'scikit-plot', 'dill', 'opencv-python', 'imbalanced-learn'],
     python_requires='>=3.7,<4',
     include_package_data=True,
     test_suite="nose.collector",
